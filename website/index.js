2bd05ce5e8515cb797c64e106a2fd331b92f9fdf
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2017-07-24 11:09:14 +0700

    11:09

--- website/index.js
+++ website/index.js
@@ -1,40 +1,48 @@
 var link = "http://158.108.165.223/data/fukseed/";
+var object = ["air", "smoke", "temperature", "waterflow", "soilmosture"];
 
 $(document).ready(function() {
   $("#app").html();
+
+  setInterval(function() {
+    update();
+  }, 1000);
 });
 
-function recieveAirPressure() {
-  var air = read("air");
-  // update
-}
-
-function recieveSmoke() {
-    var smoke = read("smoke");
-    //update
-}
-
-function recieveTemperature() {
-    var temperature = read("temperature");
-    //update
-}
-
-function recieveWaterFlow() {
-    var waterFlow = read("waterflow");
-    //update
-}
-
-function recieveSoilMosture() {
-    var soilMosture = read("soilmosture");
-    //update
-}
-
-function read(url) {
-  $.ajex({
+function read(url, work) {
+  $.ajax({
     url: link + url
   })
     .done(function(data) {
-      return data;
+      work(data);
     })
     .fail();
+}
+
+function update() {
+  object.forEach(function(item, index) {
+    read(item, function(data) {
+      $("#" + item).html(data);
+      $("#" + item + "-progress").attr(
+        "style",
+        "width: " + parseInt(data) + "%;"
+      );
+      var width =
+        parseFloat($("#" + item + "-progress").css("width")) /
+        parseFloat($("#progress").css("width")) *
+        100;
+      
+      $("#" + item + "-progress").removeClass("progress-bar-success");
+      $("#" + item + "-progress").removeClass("progress-bar-warning");
+      $("#" + item + "-progress").removeClass("progress-bar-danger");
+
+      if (width >= 66) {
+        $("#" + item + "-progress").addClass("progress-bar-danger");
+      } else if (width >= 33) {
+        $("#" + item + "-progress").addClass("progress-bar-warning");
+      } else {
+        $("#" + item + "-progress").addClass("progress-bar-success");
+      }
+    });
+  });
 }
