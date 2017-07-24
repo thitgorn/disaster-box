f6d288822365b8872514502316e895ba8f84305d
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2017-07-24 09:52:43 +0700

    init script

--- website/index.js
+++ website/index.js
@@ -1,3 +1,40 @@
+var link = "http://158.108.165.223/data/fukseed/";
+
 $(document).ready(function() {
-  $("#app").html(structer());
+  $("#app").html();
 });
+
+function recieveAirPressure() {
+  var air = read("air");
+  // update
+}
+
+function recieveSmoke() {
+    var smoke = read("smoke");
+    //update
+}
+
+function recieveTemperature() {
+    var temperature = read("temperature");
+    //update
+}
+
+function recieveWaterFlow() {
+    var waterFlow = read("waterflow");
+    //update
+}
+
+function recieveSoilMosture() {
+    var soilMosture = read("soilmosture");
+    //update
+}
+
+function read(url) {
+  $.ajex({
+    url: link + url
+  })
+    .done(function(data) {
+      return data;
+    })
+    .fail();
+}
